edfaf4abc16e9f8daf4e6507f706f86169d26818
Patryk D <tomr33d32@gmail.com>
2019-03-03 23:19:01 +0100

    Readme | Added Readme and changed main class name

--- src/main/java/com/example/springLoan/SpringLoanRestAPI.java
+++ src/main/java/com/example/springLoan/SpringLoanRestAPI.java
@@ -6,9 +6,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class SpringLoan18112018Application {
+public class SpringLoanRestAPI {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringLoan18112018Application.class, args);
+		SpringApplication.run(SpringLoanRestAPI.class, args);
 	}
 }
