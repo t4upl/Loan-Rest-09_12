7e94a67f7a6bb35917a2e7bd49e16b17d31bd90a
Patryk D <tomr33d32@gmail.com>
2019-03-17 23:32:48 +0100

    SpringLoanRestAPI | Chnaged name of context load test.

--- src/test/java/com/example/springLoan/SpringLoanRestAPITest.java
+++ src/test/java/com/example/springLoan/SpringLoanRestAPITest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringLoan18112018ApplicationTests {
+public class SpringLoanRestAPITest {
 
 	@Test
 	public void contextLoads() {
